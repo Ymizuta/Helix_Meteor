f73a21e0afc740d3fab54570bfd9b184afda2f34
Ymizuta <suiden.53@gmail.com>
2018-09-18 00:33:56 +0900

    MoveスクリプトをPlayerスクリプトに名前修正。

---
+++ Helix_Meteor/Assets/Script/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float player_posz;           //
+    public float fall_speed = 0.01f;    //前方に移動するスピード
+
+    public float move_speed = 2f;
+    public float width = 3f;
+    public float hieght = 3f;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    //プレイヤーが前方に移動
+    void Move()
+    {
+        float x = Mathf.Cos(Time.time * speed) * width;
+        float y = Mathf.Sin(Time.time * speed) * height;
+        gameObject.transform.Translate(x, y, player_posz);
+        player_posz += fall_speed;
+    }
+}
