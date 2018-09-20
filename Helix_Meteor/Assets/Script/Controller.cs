ebfe6b78bab79daf3ba3f5c4801689f4b504261b
Ymizuta <suiden.53@gmail.com>
2018-09-21 02:31:12 +0900

    コントローラークラス(Contoroller.cs)からプレイヤークラス(Player.cs)のMove関数を呼び出すよう実装

--- Helix_Meteor/Assets/Script/Controller.cs
+++ Helix_Meteor/Assets/Script/Controller.cs
@@ -2,15 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Controller : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    [SerializeField] Player player_ = null; // エディターからアタッチする
+    [SerializeField] Camera meteor_camera_ = null; // エディターからアタッチする
+
+    public enum DIRECTION
+    {
+        Right,
+        Left
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        //左右入力取得
+        DIRECTION direction;
+        //右入力
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            direction = DIRECTION.Right;
+            player_.Move(direction);
+        }
+        else
+        //左入力
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            direction = DIRECTION.Left;
+            player_.Move(direction);
+        }
+    }
 }
--- Helix_Meteor/Assets/Script/Player.cs
+++ Helix_Meteor/Assets/Script/Player.cs
@@ -4,24 +4,17 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-    public float player_posz;           //
-    public float fall_speed = 0.001f;    //前方に移動するスピード
-    public float add_speed = 0.5f;
+    [SerializeField] Controller Controller_ = null; // エディターからアタッチする
 
+    public float player_posz;           //
+    public float fall_speed = 0.001f;   //前方に移動する初期速度
+    public float add_speed = 0.2f;      //時間経過で加算される速度
+
+    //円周上を移動するための変数
     float player_degree;
-    float move_speed;
+    float move_speed = 5f;              //左右移動の速度
     float height = 1f;
     float width = 1f;
-    enum DIRECTION
-    {
-        Right,
-        Left
-    }
-
-    // Use this for initialization
-    void Start()
-    {
-    }
 
     // Update is called once per frame  
     void Update()
@@ -29,29 +22,12 @@ public class Player : MonoBehaviour {
         //直進
         player_posz += fall_speed;
         gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, player_posz);
-
-        //左右入力取得
-        DIRECTION direction;
-        //右
-        if(Input.GetAxis("Horizontal")>0)
-        {
-            direction = DIRECTION.Right;
-            Move(direction);
-        }
-        else
-        //左
-        if(Input.GetAxis("Horizontal") < 0)
-        {
-            direction = DIRECTION.Left;
-            Move(direction);
-        }
-
         //時間経過でスピードアップ
         fall_speed = SpeedUp(add_speed);
-
     }
 
-    void Move(DIRECTION direction)
+    //プレイヤーの左右移動関数
+    public void Move(Controller.DIRECTION direction)
     {
         float x;
         float y;
@@ -59,16 +35,16 @@ public class Player : MonoBehaviour {
         switch (direction)
         {
             //右
-            case DIRECTION.Right:
-                player_degree += 1;
+            case Controller.DIRECTION.Right:
+                player_degree += move_speed;
                 x = Mathf.Cos(player_degree * Mathf.Deg2Rad) * 3;
                 y = Mathf.Sin(player_degree * Mathf.Deg2Rad) * 3;
                 player_posz += fall_speed;
                 gameObject.transform.position = new Vector3(x, y, player_posz);
-                return;       
+                return;
             //左 
-            case DIRECTION.Left:
-                player_degree -= 1;
+            case Controller.DIRECTION.Left:
+                player_degree -= move_speed;
                 x = Mathf.Cos(player_degree * Mathf.Deg2Rad) * 3;
                 y = Mathf.Sin(player_degree * Mathf.Deg2Rad) * 3;
                 player_posz += fall_speed;
@@ -78,13 +54,14 @@ public class Player : MonoBehaviour {
                 return;
         }
     }
-
+    
+    //プレイヤー加速関数
     public float SpeedUp(float add_speed)
     {
         fall_speed += add_speed * Time.deltaTime;
         return fall_speed;
     }
-
+    //衝突処理
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("衝突！");
